f58a60d3c2f352fc506384dc977ca73795208d40
Echo <89703312@qq.com>
2017-09-15 10:23:23 +0800

    动态读取port
    石若非帮忙改下文件的路径，我现在还是用的相对路径，你改成读取根目录下的文件

--- analyser/analyser.py
+++ analyser/analyser.py
@@ -1,12 +1,18 @@
 from flask import Flask
+import ConfigParser
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return "Hello!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    # modify the port dynamically
+    config = ConfigParser.ConfigParser()
+    config.read("../config.ini")
+    remote_port = config.get("remote", "port")
+
+    app.run(host = '0.0.0.0', port = int(remote_port) , debug = True)
