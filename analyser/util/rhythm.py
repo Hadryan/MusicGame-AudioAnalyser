42981798c92f0e72713478b0f49ce6224311dfcf
包纸 <141250108@smail.nju.edu.cn>
2017-09-27 20:25:51 +0800

    用新的进程加载音频与解析数据

--- analyser/util/rhythm.py
+++ analyser/util/rhythm.py
@@ -1,28 +1,30 @@
+#encoding:utf-8
 import essentia
 import json
 from essentia.streaming import *
+from multiprocessing import Process, Manager
+
+manager = Manager()
+result = manager.list()
 
 def rhythm_extractor(filename):
-    pool = essentia.Pool()
+    p = Process(target=__rhythm_extractor_process, args=(filename,))
+    p.start()
+    p.join()
+    # 这里的实现不是很好，问题是python2里的manager.dict()有bug，无法共享内存，所以用list.append代替，再取第0个元素
+    return result[0]
 
-    loader = AudioLoader(filename=filename)
-    mixer = MonoMixer()
+def __rhythm_extractor_process(filename):
+    pool = essentia.Pool()
+    loader = MonoLoader(filename=filename)
     bt = RhythmExtractor2013()
-    loader.audio >> mixer.audio
-    loader.numberChannels >> mixer.numberChannels
-    loader.sampleRate >> None
-    loader.md5 >> None
-    loader.bit_rate >> None
-    loader.codec >> None
-    mixer.audio >> bt.signal
+    loader.audio >> bt.signal
     bt.bpm >> (pool, 'bpm')
     bt.ticks >> (pool, 'ticks')
     bt.confidence >> (pool, 'confidence')
     bt.estimates >> (pool, 'estimates')
     bt.bpmIntervals >> (pool, 'bpmIntervals')
     essentia.run(loader)
-
-    result = {'bpm' : pool['bpm'], 'ticks' : list(pool['ticks']), 'confidence' : pool['confidence'], 'estimates' : list(pool['estimates']), 'bpmIntervals' : list(pool['bpmIntervals'])}
-    print result
-
-    return result
+    result.append({'bpm': pool['bpm'], 'ticks': list(pool['ticks']), 'confidence': pool['confidence'],
+              'estimates': list(pool['estimates']), 'bpmIntervals': list(pool['bpmIntervals'])})
+    pool.clear()
